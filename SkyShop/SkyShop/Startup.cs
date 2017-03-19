b7b4324836d84dcc76d849cba8589b9cea1497cf
Hà Văn Quí <havanqui1993@gmail.com>
2017-03-19 12:08:15 +0700

    First project

---
+++ SkyShop/SkyShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkyShop.Startup))]
+namespace SkyShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
